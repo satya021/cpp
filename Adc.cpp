821229ac032ca84ede183f4ac85b550fa7b2fd7d
satyabrata <92144172+satya021@users.noreply.github.com>
2022-02-04 23:30:49 +0530

    Create Adc.cpp

---
+++ Adc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  double num1, num2, product;
+
+  cout << "Enter two numbers: ";
+
+ating point numbers in num1 and num2 respectively
+
+  cin >> num1 >> num2;
+
+ 
+
+  
+
+  product = num1 * num2;  
+
+  cout << "Product = " << product;    
+
+    
+
+  return 0;
+
+}
